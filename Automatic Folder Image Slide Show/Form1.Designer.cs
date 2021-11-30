b51ce805e689f422337b4c4ac0bcc96fd4e2adfd
MOOICT <MOOICT@DESKTOP-OQHBRE0>
2021-11-30 06:55:16 +0000

    Add project files.

---
+++ Automatic Folder Image Slide Show/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace Automatic_Folder_Image_Slide_Show
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblFileInfo = new System.Windows.Forms.Label();
+            this.imageViewer = new System.Windows.Forms.PictureBox();
+            this.slideShowTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.imageViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(352, 619);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(111, 42);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.PlayStopSlideShow);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(12, 619);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.BrowseForImages);
+            // 
+            // lblFileInfo
+            // 
+            this.lblFileInfo.Location = new System.Drawing.Point(64, 698);
+            this.lblFileInfo.Name = "lblFileInfo";
+            this.lblFileInfo.Size = new System.Drawing.Size(640, 23);
+            this.lblFileInfo.TabIndex = 2;
+            this.lblFileInfo.Text = "File Information";
+            this.lblFileInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // imageViewer
+            // 
+            this.imageViewer.Location = new System.Drawing.Point(12, 12);
+            this.imageViewer.Name = "imageViewer";
+            this.imageViewer.Size = new System.Drawing.Size(780, 584);
+            this.imageViewer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageViewer.TabIndex = 3;
+            this.imageViewer.TabStop = false;
+            // 
+            // slideShowTimer
+            // 
+            this.slideShowTimer.Tick += new System.EventHandler(this.PlaySlideShowTimerEvent);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(516, 628);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Speed: ";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(567, 624);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(36, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1x";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.SpeedChangeEvent);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(628, 624);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(36, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2x";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.SpeedChangeEvent);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(682, 624);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(36, 17);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "3x";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.SpeedChangeEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 761);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imageViewer);
+            this.Controls.Add(this.lblFileInfo);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "Form1";
+            this.Text = "Automatic Image Slide Show App";
+            ((System.ComponentModel.ISupportInitialize)(this.imageViewer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblFileInfo;
+        private System.Windows.Forms.PictureBox imageViewer;
+        private System.Windows.Forms.Timer slideShowTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ Automatic Folder Image Slide Show/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO; // this one is needed for reading external files
+
+namespace Automatic_Folder_Image_Slide_Show
+{
+
+    // created by MOOICT.COM 2021
+    // for edcuational purposes only
+    public partial class Form1 : Form
+    {
+
+        List<string> filteredFiles;
+        FolderBrowserDialog FolderBrowser = new FolderBrowserDialog();
+
+        int counter = -1;
+        int timerInterval = 1000;
+        bool isPlaying = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            radioButton1.Checked = true;
+            slideShowTimer.Interval = timerInterval;
+            btnPlay.Enabled = false;
+
+        }
+
+        private void BrowseForImages(object sender, EventArgs e)
+        {
+
+            counter = -1;
+            isPlaying = false;
+            slideShowTimer.Stop();
+            btnPlay.Text = "Play";
+
+            DialogResult result = FolderBrowser.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                filteredFiles = Directory.GetFiles(FolderBrowser.SelectedPath, "*.*")
+                .Where(file => file.ToLower().EndsWith("jpg") || file.ToLower().EndsWith("gif")
+                || file.ToLower().EndsWith("png") || file.ToLower().EndsWith("bmp")).ToList();
+
+                lblFileInfo.Text = "Folder loaded - Now Press Play!";
+
+                btnPlay.Enabled = true;
+
+            }
+
+
+
+        }
+
+        private void PlayStopSlideShow(object sender, EventArgs e)
+        {
+            if (isPlaying == false)
+            {
+                btnPlay.Text = "Stop";
+                slideShowTimer.Start();
+                isPlaying = true;
+            }
+            else
+            {
+                btnPlay.Text = "Play";
+                isPlaying = false;
+                slideShowTimer.Stop();
+            }
+        }
+
+        private void PlaySlideShowTimerEvent(object sender, EventArgs e)
+        {
+            counter += 1;
+
+            if (counter >= filteredFiles.Count)
+            {
+                counter = -1;
+            }
+            else
+            {
+                imageViewer.Image = Image.FromFile(filteredFiles[counter]);
+                lblFileInfo.Text = filteredFiles[counter].ToString();
+            }
+        }
+
+        private void SpeedChangeEvent(object sender, EventArgs e)
+        {
+
+            RadioButton tempRadioButton = sender as RadioButton;
+
+            switch (tempRadioButton.Text.ToString())
+            {
+                case "1x":
+                    timerInterval = 3000;
+                    break;
+                case "2x":
+                    timerInterval = 2000;
+                    break;
+                case "3x":
+                    timerInterval = 1000;
+                    break;
+            }
+
+            slideShowTimer.Interval = timerInterval;
+
+
+
+        }
+    }
+}
